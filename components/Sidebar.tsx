a29044218e86bb523ad3c26730720420e1b2dc0a
Sagar1329 <sagargowda1329@gmail.com>
2024-10-08 23:46:54 +0530

    Implement sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,11 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 const Sidebar = ({ user }: SiderbarProps) => {
+    const pathname=usePathname()
     return (
         <section className='sidebar'>
             <nav className='flex flex-col gap-4'>
@@ -20,6 +25,33 @@ const Sidebar = ({ user }: SiderbarProps) => {
                     </h1>
 
                 </Link>
+                {sidebarLinks.map((item) => {
+                    const isActive = pathname=== item.route || pathname.startsWith(`${item.route}/`)
+                    return (
+                        <Link
+                            key={item.label}
+                            href={item.route}
+                        className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+                        >
+
+<div className='relative size-6'>
+    <Image
+    src={item.imgURL}
+    alt={item.label}
+    fill
+
+    className={cn({'brightness-[3] invert-0':isActive})}
+    />
+
+</div>
+<p className={cn('sidebar-label',{'!text-white':isActive})}>
+    {item.label}
+</p>
+
+
+                        </Link>
+                    )
+                })}
 
             </nav>
         </section>
