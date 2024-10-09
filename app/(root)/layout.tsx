d7a41180db99ff056cb6f95e57437cc9d67c9abb
Sagar1329 <sagargowda1329@gmail.com>
2024-10-09 17:35:19 +0530

    Implement left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,36 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const loggedIn = { firstName: "Sagar",lastName:"gowda"} 
+    const loggedIn = { firstName: "Sagar", lastName: "gowda" }
 
     return (
         <main className="flex h-screen w-full font-inter">
             <Sidebar
                 user={loggedIn}
-             />
-            {children}
+            />
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image
+                        src="/icons/logo.svg"
+                        width={30}
+                        height={30}
+                        alt="menu icon"
+                    />
+                    <div>
+                    <MobileNav user={loggedIn} />
+                
+
+                    </div>
+                </div>
+                {children}
+
+            </div>
         </main>
     );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,11 +5,12 @@ import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
 const Sidebar = ({ user }: SiderbarProps) => {
-    const pathname=usePathname()
+    const pathname = usePathname()
     return (
         <section className='sidebar'>
             <nav className='flex flex-col gap-4'>
-                <Link href='/' className='mb-12 
+                <Link href='/' className='
+                flex mb-12 
         cursor-pointer
          items-center
           gap-2'>
@@ -26,34 +27,37 @@ const Sidebar = ({ user }: SiderbarProps) => {
 
                 </Link>
                 {sidebarLinks.map((item) => {
-                    const isActive = pathname=== item.route || pathname.startsWith(`${item.route}/`)
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                     return (
                         <Link
                             key={item.label}
                             href={item.route}
-                        className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+                            className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
                         >
 
-<div className='relative size-6'>
-    <Image
-    src={item.imgURL}
-    alt={item.label}
-    fill
+                            <div className='relative size-6'>
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
 
-    className={cn({'brightness-[3] invert-0':isActive})}
-    />
+                                    className={cn({ 'brightness-[3] invert-0': isActive })}
+                                />
 
-</div>
-<p className={cn('sidebar-label',{'!text-white':isActive})}>
-    {item.label}
-</p>
+                            </div>
+                            <p className={cn('sidebar-label', { '!text-white': isActive })}>
+                                {item.label}
+                            </p>
 
 
                         </Link>
                     )
                 })}
 
+                USER
+
             </nav>
+            Footer
         </section>
     )
 }
